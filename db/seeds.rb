3a748a33c67628f4dbd98931c116f6473bad1bc5
kenichi.arita <kenichi.arita@kiramex.com>
2021-07-12 02:06:55 +0000

    for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(name: 'taro', email: 'taro@techacademy.jp', password: 'taro1234')
+User.create(name: 'hanako', email: 'hanako@techacademy.jp', password: 'hanako1234')
+
+(1..100).each do |number|
+  rand = rand(2) + 1
+  user = User.find(rand)
+  user.microposts.build(content: 'test content ' + number.to_s)
+  user.save
+end
+
+(1..20).each do |number|
+  rand = rand(2) + 1
+  user = User.find(rand)
+  randm = rand(100) + 1
+  user.favorites.find_or_create_by(micropost_id: randm)
+  user.save
+end
