32995ea25e382acb9cfec8a6e80a8062de5199b7
kenichi.arita <kenichi.arita@kiramex.com>
2021-07-12 01:44:29 +0000

    add favorites

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,6 +39,12 @@ class UsersController < ApplicationController
     counts(@user)
   end
 
+  def likes
+    @user = User.find(params[:id])
+    @pagy, @favorites = pagy(@user.favorite_microposts)
+    counts(@user)
+  end
+
   private
 
   def user_params
--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,11 @@
 class Micropost < ApplicationRecord
   belongs_to :user
   
+  has_many :favorites
+  
   validates :content, presence: true, length: { maximum: 255 }
+  
+  def favoriting?(user)
+    self.favorites.include?(user)
+  end
 end
